9beeb4c7a5a06c9f7187c1b63436d535181168bb
Rodrigo Cajal <110480381+Rodrigo-Cajal@users.noreply.github.com>
2023-11-27 16:25:55 -0300

    menu-hamb

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import { Home } from './screens/home/home'
-
+import { MenuProvider } from './context/menuHamb';
 
 
 function App() {
 
-  return  <Home/>
-
+  return(
+ <MenuProvider>
+ <div>
+  <Home/>
+  </div>
+ </MenuProvider>
+  );
 
 }
 
---
+++ src/context/menuHamb.jsx
@@ -0,0 +1,22 @@
+// MenuContext.jsx
+import { createContext, useContext, useState } from 'react';
+
+const MenuContext = createContext();
+
+export const useMenu = () => {
+  return useContext(MenuContext);
+};
+
+export const MenuProvider = ({ children }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <MenuContext.Provider value={{ isMenuOpen, toggleMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
